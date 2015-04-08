fa4c3e4a7d426f2873e6849bc1b09639d49b8c89
aungkhant <june27akz2009@gmail.com>
2015-04-07 21:31:07 -0800

    Just first step for styling
    Add bootstrap.min.css

--- footer.php
+++ footer.php
@@ -8,14 +8,19 @@
  */
 ?>
 
+                </div><!-- .panel-body -->
+            </div><!-- .panel -->
+        </div><!-- .container -->
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'april-white' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'april-white' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'april-white' ), 'April White', '<a href="http://www.johnthelinux.com" rel="designer">John Reginald</a>' ); ?>
-		</div><!-- .site-info -->
+        <div class="container">
+    		<div class="site-info">
+    			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'april-white' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'april-white' ), 'WordPress' ); ?></a>
+    			<span class="sep"> | </span>
+    			<?php printf( __( 'Theme: %1$s by %2$s.', 'april-white' ), 'April White', '<a href="http://www.johnthelinux.com" rel="designer">John Reginald</a>' ); ?>
+    		</div><!-- .site-info -->
+        </div><!-- .container -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
